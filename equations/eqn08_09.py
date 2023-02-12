a19777a7789f975716be1fac8a4263108b70a4ad
meticulousdev <meticulousdev@gmail.com>
2023-02-12 16:54:44 +0900

    eqn08_09 py and ipynb

---
+++ equations/eqn08_09.py
@@ -0,0 +1,53 @@
+import random
+
+import tensorflow as tf
+import tensorflow.keras.backend as K
+
+from tensorflow.keras.layers import Dense, Permute, Softmax, Activation, Add
+
+batch_size = 7
+T = 5
+n = 4
+m = 3
+
+ele01 = [random.random() for _ in range(n)]
+ele02 = [ele01 for _ in range(T)]
+ele03 = [ele02 for _ in range(batch_size)]
+
+X = tf.constant(ele03, dtype=tf.float32)
+
+hs_ele01 = [random.random() for _ in range(m)]
+hs_ele02 = [hs_ele01 for _ in range(batch_size)]
+hidden_state = tf.constant(hs_ele02, dtype=tf.float32)
+
+cs_ele01 = [random.random() for _ in range(m)]
+cs_ele02 = [cs_ele01 for _ in range(batch_size)]
+cell_state = tf.constant(cs_ele02, dtype=tf.float32)
+
+# X (batch size, T, n)
+#
+# concat_hs (batach, n, 2m)
+# hs (batch size, n, T)
+# ux (batch size, n, T)
+#
+# add_hs_ux (batch size, n, T)
+# tanh_act_add (batch size, n, T)
+# e_add (batch size, n, 1)
+#
+# attn_add (batch size, 1, n)
+
+# Eqn. (8)
+n = X.shape[2]
+
+concat_hs = K.repeat(tf.concat([hidden_state, cell_state], axis=-1), n)
+hs = Dense(T)(concat_hs)
+ux = Dense(T)(Permute((2, 1))(X))
+
+add_hs_ux = Add()([hs, ux])
+tanh_act = Activation(activation='tanh')(add_hs_ux)
+e = Dense(1)(tanh_act)
+
+# Eqn. (9)
+attn = Softmax()(Permute((2, 1))(e))
+
+print(attn)
