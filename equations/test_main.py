b38f1422105c74152955b9182ed365f2e7d4175c
meticulousdev <meticulousdev@gmail.com>
2023-03-11 10:02:19 +0900

    test encoder and decoder

--- equations/test_main.py
+++ equations/test_main.py
@@ -1,13 +1,28 @@
-from test_model_merged_class import test_encoder_merged_class
-from test_model_separated_class import test_encoder_seprated_class
+from test_model_merged_class import test_encoder_merged_class, test_decoder_merged_class
+from test_model_separated_class import test_encoder_separated_class, test_decoder_separated_class
 
 if __name__ == "__main__":
-    batch_size = 7
+    batch_size = 12
     T = 5
     n = 4
-    m = 3
+    p = 6
+    m = 4
 
-    ret_sep = test_encoder_seprated_class(batch_size, T, n, m)
-    ret_mer = test_encoder_merged_class(batch_size, T, n, m)
+    y_dim = 3
 
-    print(sum(ret_sep - ret_mer))
+    ret_en_sep = test_encoder_separated_class(batch_size, T, n, m)
+    ret_en_mer = test_encoder_merged_class(batch_size, T, n, m)
+    print(sum(ret_en_sep - ret_en_mer))
+
+    ret_de_sep = test_decoder_separated_class(batch_size, T, m, p, y_dim)
+    ret_de_mer = test_decoder_merged_class(batch_size, T, m, p, y_dim)
+    print(sum(ret_de_sep - ret_de_mer))
+
+    # tf.Tensor(
+    # [[0. 0. 0. 0.]
+    # [0. 0. 0. 0.]
+    # [0. 0. 0. 0.]
+    # [0. 0. 0. 0.]
+    # [0. 0. 0. 0.]], shape=(5, 4), dtype=float32)
+
+    # tf.Tensor([0. 0. 0.], shape=(3,), dtype=float32)
